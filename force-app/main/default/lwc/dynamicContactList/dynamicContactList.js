b390f50ce5b61db56bc06d798faf8931819aa333
Debdeep GHosh <ghosh.debdeep94@outlook.com>
2026-02-03 12:42:10 +0530

    Update to Some Components

---
+++ force-app/main/default/lwc/dynamicContactList/dynamicContactList.js
@@ -0,0 +1,31 @@
+import { LightningElement } from 'lwc';
+import getContacts from '@salesforce/apex/ContactManager.getContacts';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+
+export default class DynamicContactList extends LightningElement {
+
+    numberOfRecords;
+    contacts;
+
+    handleChange(event){
+        this.numberOfRecords = event.target.value;
+    }
+
+    getContacts(){
+        getContacts({records:this.numberOfRecords})
+        .then(response=>{
+            this.contacts=response;
+
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Succesfully Retrieved Records',
+                    variant: 'success'
+                })
+            );
+        })
+        .catch(error=>{
+                console.log(error);
+            });
+        }
+    }
