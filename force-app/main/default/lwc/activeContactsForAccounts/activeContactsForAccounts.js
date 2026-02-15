49bcda211e86294b641e64710a801933c629eaf8
Debdeep GHosh <ghosh.debdeep94@outlook.com>
2026-02-16 00:38:57 +0530

    ActiveConatctsForSalesforce

---
+++ force-app/main/default/lwc/activeContactsForAccounts/activeContactsForAccounts.js
@@ -0,0 +1,30 @@
+import { LightningElement, api } from 'lwc';
+import activeContacts from '@salesforce/apex/ContactManager.activeContacts';
+
+export default class ActiveContactsForAccounts extends LightningElement {
+
+    @api recordId;
+    contacts = [];
+    error = null;
+    showContacts = false;
+
+    handleHideContacts() {
+        this.showContacts = false;
+    }
+
+    handleShowContacts(){
+        activeContacts({recordId: this.recordId, active: true})
+        .then(response=>{
+            this.contacts = response;
+            this.showContacts = true;
+        })
+        .catch(error=>{
+            this.error = error;
+            console/log(error);
+        })
+    }
+
+    handleClearError(){
+        this.error = null;
+    }
+}
